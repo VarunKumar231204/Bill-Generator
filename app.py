018aedc93c126bae759e135f37a698e91917d719
varun gautam <varungautam2312@gmail.com>
2025-04-12 12:02:01 +0530

    After Commit

--- app.py
+++ app.py
@@ -146,6 +146,6 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))  # <- default to 10000 if not set
     app.run(host="0.0.0.0", port=port)
 
